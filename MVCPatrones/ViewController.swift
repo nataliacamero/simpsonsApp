9ffe7a2cace4b9ac3277c8ab57cbbfe51f1a4096
Natalia Camero Carreño <nataliacameroc@gmail.com>
2023-10-02 20:54:20 +0200

    Initial Commit

---
+++ MVCPatrones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCPatrones
+//
+//  Created by Natalia Camero on 2/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
